64d84d9d51ba6080b20c7719b529ecdc76d7322f
Template Janitor <template.janitor@example.com>
2023-10-09 12:50:01 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -4,7 +4,7 @@ include(CMakeDependentOption)
 include(CheckCXXCompilerFlag)
 
 
-macro(myproject_supports_sanitizers)
+macro(Vulk_supports_sanitizers)
   if((CMAKE_CXX_COMPILER_ID MATCHES ".*Clang.*" OR CMAKE_CXX_COMPILER_ID MATCHES ".*GNU.*") AND NOT WIN32)
     set(SUPPORTS_UBSAN ON)
   else()
@@ -18,183 +18,183 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(Vulk_setup_options)
+  option(Vulk_ENABLE_HARDENING "Enable hardening" ON)
+  option(Vulk_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    Vulk_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    Vulk_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  Vulk_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR Vulk_PACKAGING_MAINTAINER_MODE)
+    option(Vulk_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(Vulk_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(Vulk_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(Vulk_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(Vulk_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(Vulk_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(Vulk_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(Vulk_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(Vulk_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(Vulk_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(Vulk_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(Vulk_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(Vulk_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(Vulk_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(Vulk_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(Vulk_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(Vulk_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(Vulk_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(Vulk_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(Vulk_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(Vulk_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(Vulk_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(Vulk_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(Vulk_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(Vulk_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(Vulk_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_USER_LINKER
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      Vulk_ENABLE_IPO
+      Vulk_WARNINGS_AS_ERRORS
+      Vulk_ENABLE_USER_LINKER
+      Vulk_ENABLE_SANITIZER_ADDRESS
+      Vulk_ENABLE_SANITIZER_LEAK
+      Vulk_ENABLE_SANITIZER_UNDEFINED
+      Vulk_ENABLE_SANITIZER_THREAD
+      Vulk_ENABLE_SANITIZER_MEMORY
+      Vulk_ENABLE_UNITY_BUILD
+      Vulk_ENABLE_CLANG_TIDY
+      Vulk_ENABLE_CPPCHECK
+      Vulk_ENABLE_COVERAGE
+      Vulk_ENABLE_PCH
+      Vulk_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  Vulk_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (Vulk_ENABLE_SANITIZER_ADDRESS OR Vulk_ENABLE_SANITIZER_THREAD OR Vulk_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(Vulk_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(Vulk_global_options)
+  if(Vulk_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    Vulk_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  Vulk_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(Vulk_ENABLE_HARDENING AND Vulk_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR Vulk_ENABLE_SANITIZER_UNDEFINED
+       OR Vulk_ENABLE_SANITIZER_ADDRESS
+       OR Vulk_ENABLE_SANITIZER_THREAD
+       OR Vulk_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${Vulk_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${Vulk_ENABLE_SANITIZER_UNDEFINED}")
+    Vulk_enable_hardening(Vulk_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(Vulk_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(Vulk_warnings INTERFACE)
+  add_library(Vulk_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  Vulk_set_project_warnings(
+    Vulk_warnings
+    ${Vulk_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
     "")
 
-  if(myproject_ENABLE_USER_LINKER)
+  if(Vulk_ENABLE_USER_LINKER)
     include(cmake/Linker.cmake)
-    configure_linker(myproject_options)
+    configure_linker(Vulk_options)
   endif()
 
   include(cmake/Sanitizers.cmake)
-  myproject_enable_sanitizers(
-    myproject_options
-    ${myproject_ENABLE_SANITIZER_ADDRESS}
-    ${myproject_ENABLE_SANITIZER_LEAK}
-    ${myproject_ENABLE_SANITIZER_UNDEFINED}
-    ${myproject_ENABLE_SANITIZER_THREAD}
-    ${myproject_ENABLE_SANITIZER_MEMORY})
+  Vulk_enable_sanitizers(
+    Vulk_options
+    ${Vulk_ENABLE_SANITIZER_ADDRESS}
+    ${Vulk_ENABLE_SANITIZER_LEAK}
+    ${Vulk_ENABLE_SANITIZER_UNDEFINED}
+    ${Vulk_ENABLE_SANITIZER_THREAD}
+    ${Vulk_ENABLE_SANITIZER_MEMORY})
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(Vulk_options PROPERTIES UNITY_BUILD ${Vulk_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(Vulk_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      Vulk_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(Vulk_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    Vulk_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(Vulk_ENABLE_CLANG_TIDY)
+    Vulk_enable_clang_tidy(Vulk_options ${Vulk_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(Vulk_ENABLE_CPPCHECK)
+    Vulk_enable_cppcheck(${Vulk_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(Vulk_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    Vulk_enable_coverage(Vulk_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(Vulk_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(Vulk_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(Vulk_ENABLE_HARDENING AND NOT Vulk_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR Vulk_ENABLE_SANITIZER_UNDEFINED
+       OR Vulk_ENABLE_SANITIZER_ADDRESS
+       OR Vulk_ENABLE_SANITIZER_THREAD
+       OR Vulk_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    Vulk_enable_hardening(Vulk_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
